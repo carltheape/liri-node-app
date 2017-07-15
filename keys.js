de5e6e84b6d719fbf363e5ae53a55d29a12dbf76
Carltheape <tbfinkle@gmail.com>
2017-07-15 16:41:27 -0400

    setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<uO7v8I7T69qTcFOBQwMaUqoZz>',
+  consumer_secret: '<VJQJ2lGGAVjRpzi0oEjpoirr7sfW8AqWGzdeC0j5Ay3vh3FZRC>',
+  access_token_key: '<885486217424494594-kWJ7CN1lv8LrodFtmb0ljQVfmKOENli>',
+  access_token_secret: '<9bpoQUsMYJFP6kq5r0Aur2JyXlRKMddM6qD9grcE0p6pu>',
+}
---
+++ liri.js
@@ -0,0 +1,5 @@
+var keys = require("keys.js");
+var fs = require("fs");
+var twitter = require("twitter");
+var request = require("request");
+var spotify = require("node-spotify-api");
